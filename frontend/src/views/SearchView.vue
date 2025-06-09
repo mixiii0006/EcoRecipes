134bc3a0f927a97c0e9260912e6a12814ddeb819
Rana Helida <131280117+Rahel17@users.noreply.github.com>
2025-06-09 21:49:46 +0700

    modal + card cone

--- frontend/src/views/SearchView.vue
+++ frontend/src/views/SearchView.vue
@@ -373,7 +373,9 @@ export default {
 .food-category-grid {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  gap: 1.5rem;
+  gap: 1.5rem;          
+  padding-bottom: 2rem;
+  align-items: start;  
 }
 
 .food-category-card {
